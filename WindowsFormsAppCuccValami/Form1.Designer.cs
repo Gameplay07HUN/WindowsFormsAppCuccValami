b3a528f79a88ba3737a81f3a120f21bd1e6ae89c
Gameplay07HUN <gameplay719@gmail.com>
2023-01-16 14:08:13 +0100

    Add project files.

---
+++ WindowsFormsAppCuccValami/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace WindowsFormsAppCuccValami
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tarolo = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // tarolo
+            // 
+            this.tarolo.Dock = System.Windows.Forms.DockStyle.Right;
+            this.tarolo.Location = new System.Drawing.Point(275, 0);
+            this.tarolo.Name = "tarolo";
+            this.tarolo.Size = new System.Drawing.Size(925, 566);
+            this.tarolo.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 566);
+            this.Controls.Add(this.tarolo);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "CuccValami";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel tarolo;
+    }
+}
+
---
+++ WindowsFormsAppCuccValami/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppCuccValami
+{
+    public partial class Form1 : Form
+    {
+        int sorokSzama = 3;
+        int oszlopokSzema = 6;
+        int buttonSize = 60;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < sorokSzama; i++)
+            {
+                for (int j = 0; j < oszlopokSzema; j++)
+                {
+                    Button button=new Button();
+                    button.SetBounds(j*buttonSize,i*buttonSize,buttonSize,buttonSize);
+                    button.Text = (i * oszlopokSzema + j).ToString();
+                    button.Click += (o, ev) =>
+                    {
+                        Button gomb = (Button)o;
+                        gomb.BackColor=Color.Red;
+                        MessageBox.Show(gomb.Text);
+                    };
+                    tarolo.Controls.Add(button);                   
+                }
+            }
+        }
+    }
+}
